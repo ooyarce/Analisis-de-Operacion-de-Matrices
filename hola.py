ed455e268b2b45d2102cefb4ecf0690249d602a3
ooyarce <oioyarce@miuandes.cl>
2020-08-05 23:45:23 -0400

    Create hola.py

---
+++ hola.py
@@ -0,0 +1,21 @@
+nombre = "MCOC"
+dia = 3
+
+print("Forma 1")
+print("hola Jose es el 3 de Agosto")
+
+
+print("Forma 2")
+print("hola " + nombre + " es el " + str(dia) + " de Agosto")
+
+print("Forma 3")
+print("hola {nombre} es el {dia} de Agosto".format(dia=dia, nombre=nombre))
+
+print("Forma 4")
+print(f"hola {nombre} es el {dia} de Agosto")
+
+print("Ejemplo 2 - Forma 1")
+print("hola hola hola hola hola hola hola")
+
+print("Ejemplo 2 - Forma 1")
+print("hola "*100)
